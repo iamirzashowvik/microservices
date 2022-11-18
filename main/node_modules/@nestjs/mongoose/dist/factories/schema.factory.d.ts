aaa717cf9fc7449cc2e1574de04736cee457126d
Mirza Showvik <showvikmirza@gmail.com>
2022-11-18 16:50:01 +0600

    Delete main/node_modules directory

--- main/node_modules/@nestjs/mongoose/dist/factories/schema.factory.d.ts
+++
@@ -1,5 +0,0 @@
-import { Type } from '@nestjs/common';
-import * as mongoose from 'mongoose';
-export declare class SchemaFactory {
-    static createForClass<TClass = any, _TDeprecatedTypeArgument = any>(target: Type<TClass>): mongoose.Schema<TClass>;
-}
