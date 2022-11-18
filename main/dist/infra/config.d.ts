20b18eb93249363ffed269116212fb2a36de3c44
Mirza Showvik <showvikmirza@gmail.com>
2022-11-18 16:49:40 +0600

    Delete main/dist directory

--- main/dist/infra/config.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class AppConfigurationService {
-    private readonly _configService;
-    private readonly _connectionString;
-    get connectionString(): string;
-    constructor(_configService: ConfigService);
-    private _getConnectionStringFromEnvFile;
-}
--- main/dist/products/dto/update-product.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateProductDto } from './create-product.dto';
-declare const UpdateProductDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateProductDto>>;
-export declare class UpdateProductDto extends UpdateProductDto_base {
-}
-export {};
